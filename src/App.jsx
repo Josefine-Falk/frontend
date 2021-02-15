2d9344611258eaa9f5fb7828cb0c87f178f00d9d
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2021-02-15 11:36:37 +0100

    her

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { Redirect, Route, Router, Switch } from 'react-router';
 import './App.css';
 
 function App() {
   return (
-    <p>heeey</p>
+    <Router>
+      <Switch>
+        <Route exact path="/Forside"></Route>
+
+        <Route exact path="/Dyrene"></Route>
+
+        <Route exact path="/Kontakt"></Route>
+
+        <Route exact path="/">
+          <Redirect to="/Forside" />
+        </Route>
+
+        <Route path="/">
+          <p>Denne side findes ikke</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
