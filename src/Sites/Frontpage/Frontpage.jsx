48b744872c588a106720301ee9ee36a7a04ce66d
Daniel Krogh <daniel@kroghkommunikation.dk>
2021-02-17 10:55:48 +0100

    .

--- src/Sites/Frontpage/Frontpage.jsx
+++ src/Sites/Frontpage/Frontpage.jsx
@@ -1,8 +1,14 @@
+import style from './Frontpage.module.scss';
+import logo from '../../Assets/img/techcollege-transparent-logo-hvid.png'
+
 export function Frontpage() {
 
     return (
-        <main>
-            <p>dette er forsiden</p>
+        <main id={style.container}>
+            <div>
+                <img src={logo} alt=""/>
+                <p>Lidt tekst</p>
+            </div>
         </main>
     )
 }
