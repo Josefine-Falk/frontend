8ec972cdf06f58673a8a08998c6b7cd976a6fbb9
Jeppe Steen <61780753+Jeppe-Steen@users.noreply.github.com>
2021-02-18 09:48:18 +0100

    her

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu';
 import Style from './Navigation.module.scss';
 
 export function Navigation() {
     let { animalId } = useParams();
-
-    let history = useHistory();
-
-    const handleClick = () => {
-        history.goBack();
-    }
 
     return (
         <nav className={Style.pageNavigation}>
